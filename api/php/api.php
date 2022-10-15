e70bb89fd47e0232e895b8dd17decf71e0a5ef57
Muhammad Habib <mhabib555@gmail.com>
2022-10-15 10:52:46 +0300

    Added Php Api

---
+++ api/php/api.php
@@ -0,0 +1,41 @@
+<?php
+require_once('./data/dataObject.php');
+
+$limit = 5;
+$offset = 0;
+if(isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+}
+if(isset($_GET['offset'])) {
+    $offset = $_GET['offset'];
+}
+$links = [
+    'base' => 'http://localhost/projects/datatables/customPaginationPhpNode/api/php/api.php',
+    'context' => '',
+    'next' => "http://localhost/projects/datatables/customPaginationPhpNode/api/php/api.php?limit=$limit&offset=".$offset+$limit,
+    'self' => 'http://localhost/projects/datatables/customPaginationPhpNode/api/php/api.php' 
+];
+
+if($offset>0) {
+    $links['prev'] = "http://localhost/projects/datatables/customPaginationPhpNode/api/php/api.php?limit=$limit&offset=".$offset-$limit;
+}
+
+
+var_dump([
+    'status' => 'success',
+    '_links' => $links,
+    'results' => array_slice($data, $offset, $limit),
+    'count' => count($data),
+    'curr'
+]);
+
+
+// echo json_encode([
+//     'status' => 'success',
+//     '_links' => $links,
+//     'results' => array_slice($data, $offset, $limit),
+//     'count' => count($data),
+//     'curr'
+// ]);
+
+
